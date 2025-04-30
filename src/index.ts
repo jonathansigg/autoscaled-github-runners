c269ca1d05b7764aa15bdddc34f9489c8778bff1
Jonathan Sigg <jonathan@at-point.ch>
2025-04-30 13:24:50 +0200

    Cleanup code

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
-import { Command } from "commander";
-import { loadSetupCommands } from "./commands/setup.js";
-import { loadConfig } from "./helper/config.js";
+import { Command } from 'commander';
+import { loadSetupCommands } from './commands/setup.js';
+import { loadConfig } from './helper/config.js';
 const program = new Command();
 loadConfig().then((config) => {
 	program
-		.name("typescript-cli-tool")
-		.description("A powerful CLI tool built with TypeScript")
-		.version("1.0.0")
-		.setOptionValue("config", config);
+		.name('github-runner-cli')
+		.description('A powerful CLI tool for managing GitHub runners')
+		.version('1.0.0')
+		.setOptionValue('config', config);
 
 	loadSetupCommands(program);
-
-	program.on("option:token", () => {
-		console.log("Token set");
-	});
 
 	program.parse(process.argv);
 });
